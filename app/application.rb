ddce3f5ca8fba7762b1d1ced3954af27e8633ca6
Brandon Weaver <a7xncob@gmail.com>
2019-12-16 20:33:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,12 @@
 class Application
   
+  @@items = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    
+    if req.path.match(/items/)
     resp.finish
   end
   
